d7e33ac414e12a50265d7fc0a57d4b2ba113c069
Marcos Higildo <marcosmuangula@gmail.com>
2025-10-31 15:36:50 +0200

    feat(login): adiciona formulário funcional com validação usando Zod e React Hook Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    
-   </div> 
-  )
+    <main className="flex min-h-screen items-center justify-center bg-gray-50">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-green-600">EduTrack Moz</h1>
+        <p className="mt-2 text-gray-600">Construindo juntos, passo a passo</p>
+      </div>
+    </main>
+  );
 }
